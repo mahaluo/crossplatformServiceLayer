af923d8699a0d3a4fe13c6c20cde580c09169138
mahaluo <6709@ait.nsw.edu.au>
2020-06-09 00:43:33 +1000

    error handling

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const firebaseConfig = {
   measurementId: "G-TK9XTBNMJE"
 };
 
-app.use(cors({ origin: true}));
+app.use(cors({ origin: true }));
 
 let rawdata = fs.readFileSync('data.json');
 let records = JSON.parse(rawdata);
@@ -24,46 +24,88 @@ firebase.initializeApp(firebaseConfig);
 
 app.get("/simple-cors", cors(), (req, res) => {
   console.info("GET /simple-cors");
-  res.json({      
+  res.json({
     text: "Simple CORS requests are working. [GET]",
     records
   });
 });
 
 app.get("/public-projects", cors(), (req, res) => {
-  
+
   console.log('fetching public projects from firebase.. ')
   const projectList = [];
+
+  const getProjects = () => {
+    console.log('async init')
+
+    let data = firebase
+    .firestore()
+    .collection('projectList')
+    .doc('projects')
+    .collection('users')
+    .get().catch(function (error) {
+      console.error('failed:', error);
+      throw error;
+    }).then(snapshot => {
+      if (snapshot.empty) {
+        console.log('Public found no matching documents.');
+        return;
+      }
+      else {
+        snapshot.forEach(doc => {
+
+          let publicRef = firebase
+          .firestore()
+          .collection('projectList')
+          .doc('projects')
+          .collection('users')
+          .doc(doc.id)
+          .collection('userproject');
+
+          let query = publicRef.get()
+            .then(snapshot => {
+              console.log('user query results..');
+              if (snapshot.empty) {
+                console.log('Public found no matching documents.');
+                return;
+              }
+              snapshot.forEach(doc => {
+                if (doc.data().shared) {
+                  console.log('shared doc found');
+                  console.log(doc.data());
+                  const project = {
+                    project: {
+                      title: doc.data().title,
+                      body: doc.data().body,
+                      createdAt: moment(doc.data().createdAt.toDate()).calendar(),
+                      solved: doc.data().solved,
+                      shared: doc.data().shared,
+                      id: doc.id,
+                    }
+                  }
+                  projectList.push(project);
+                }
+              });
+
+              console.log('end of user foreach.. ');
+            })
+            .catch(err => {
+              console.log('Error getting documents', err);
+            }); 
+        });
+      }
+    });
+  }
+
   try {
     getProjects();
+    setTimeout(() => {
+      res.send(projectList);
+    }, 3200);
   }
-  catch(error) {
+  catch (error) {
     console.error(error);
   }
-
-  async function getProjects() {
-    console.log('async init')
-   
-    let publicRef = firebase.firestore().collection('projectList');
-    let query = publicRef.where('shared', '==', true).get()
-    .then(snapshot => {
-      if (snapshot.empty) {
-        console.log('No matching documents.');
-        return;
-      }  
-
-      snapshot.forEach(doc => {
-        console.log(doc.id, '=>', doc.data());
-
-      });
-    })
-    .catch(err => {
-      console.log('Error getting documents', err);
-    });
-    
-  }
-
-
 })
 
 app.get("/user-projects", cors(), (req, res) => {
@@ -71,43 +113,45 @@ app.get("/user-projects", cors(), (req, res) => {
   console.log('fetching users projects from firebase.. ')
   const projectList = [];
   try {
-    getProjects();    
+    getProjects();
   }
-  catch(error) {
+  catch (error) {
     console.error(error);
   }
 
   async function getProjects() {
     const data = await firebase
-    .firestore()
-    .collection('projectList')
-    .doc('projects')
-    .collection(req.headers.user)  
-    .get().catch(function(error) {             
-      console.error('failed:', error);
-      throw error;
-    })
+      .firestore()
+      .collection('projectList')
+      .doc('projects')
+      .collection('users')
+      .doc(req.headers.user)
+      .collection('userproject')
+      .get().catch(function (error) {
+        console.error('failed:', error);
+        throw error;
+      })
 
     data.forEach((doc) => {
 
       const project = {
         project: {
-        title: doc.data().title,
-        body: doc.data().body,
-        createdAt: moment(doc.data().createdAt.toDate()).calendar(),
-        solved: doc.data().solved,
-        shared: doc.data().shared,   
-        id: doc.id,
+          title: doc.data().title,
+          body: doc.data().body,
+          createdAt: moment(doc.data().createdAt.toDate()).calendar(),
+          solved: doc.data().solved,
+          shared: doc.data().shared,
+          id: doc.id,
         }
       }
-      
+
       projectList.push(project);
     });
 
     console.log('sending list of projects to client.. ')
-    
+
     res.send(projectList);
-  }    
+  }
 })
 
 app.get("/project-comments", cors(), (req, res) => {
@@ -115,74 +159,112 @@ app.get("/project-comments", cors(), (req, res) => {
   const commentList = [];
 
   try {
-    getComments();    
+    getComments();
   }
-  catch(error) {
+  catch (error) {
     console.error(error);
   }
 
   async function getComments() {
     const data = await firebase
-    .firestore()
-    .collection('projectList')
-    .doc('projects')
-    .collection(req.headers.user)
-    .doc(req.headers.id)
-    .collection('comments')      
-    .get().catch(function(error) {             
-      console.error('failed:', error);
-      throw error;
-    })
+      .firestore()
+      .collection('projectList')
+      .doc('projects')
+      .collection('users')
+      .doc(req.headers.user)
+      .collection('userproject')
+      .doc(req.headers.id)
+      .collection('comments')
+      .get().catch(function (error) {
+        console.error('failed:', error);
+        throw error;
+      })
 
     data.forEach((doc) => {
 
       const comment = {
         comment: {
-        comment: doc.data().comment,  
-        id: doc.id,
-        createdAt: moment(doc.data().createdAt.toDate()).calendar(),
+          comment: doc.data().comment,
+          id: doc.id,
+          createdAt: moment(doc.data().createdAt.toDate()).calendar(),
         }
       }
-      
+
       commentList.push(comment);
     });
 
     console.log('sending list of comments to client.. ')
     res.send(commentList);
-  }   
+  }
 
 
 
 })
-
-
 
 app.get("/auth-user", cors(), (req, res) => {
   console.log('signing in user.. ');
   console.log(req.headers.email, req.headers.password)
   try {
     firebase
-    .auth()
-    .signInWithEmailAndPassword(req.headers.email, req.headers.password)
-    .then(function (user) {
-      console.log('user: ' + JSON.stringify(user.user.uid) + ' signed in.. ');
-      res.send(JSON.stringify(user.user.uid));
+      .auth()
+      .signInWithEmailAndPassword(req.headers.email, req.headers.password)
+      .then(function (user) {
+        console.log('user: ' + JSON.stringify(user.user.uid) + ' signed in.. ');
+        res.send(JSON.stringify(user.user.uid));
+      })
+      .catch(function (error) {
+        console.log(error);
+        res.send({ error: error })
+      });
+  } catch (error) {
+    console.log(error);
+    res.send({ error: error })
+  }
+})
+
+app.get("/post-comment", cors(), (req, res) => {
+  console.log('adding comment to project: ' + req.headers.id);
+
+  console.log('comment: ' + req.headers.comment);
+  const createdAt = new Date();
+  console.log('createdAt: ' + createdAt);
+
+  const newComment = {
+    comment: req.headers.comment,
+    createdAt: createdAt
+  }
+  try {
+    firebase
+    .firestore()
+    .collection('projectList')
+    .doc('projects')
+    .collection('users')
+    .doc(req.headers.user)
+    .collection('userproject')
+    .doc(req.headers.id)
+    .collection('comments')
+    .add({
+      ...newComment
     })
-    .catch(function (error) {
-      console.log(error);
+    .then(() => {
+      console.log("added comment");
+      res.send({success: 'success'});
+    })
+    .catch((err) => {
+      console.log(err);
       res.send({error: error})
     });
   } catch (error) {
-    console.log(error);
+    console.error(error);
     res.send({error: error})
   }
 })
 
 if (!module.parent) {
-  
-    const port = process.env.PORT || 3000;
-  
-    app.listen(port, () => {
-      console.log("Express server listening on port " + port + ".");
-    });
+
+  const port = process.env.PORT || 3000;
+
+  app.listen(port, () => {
+    console.log("Express server listening on port " + port + ".");
+  });
 }
