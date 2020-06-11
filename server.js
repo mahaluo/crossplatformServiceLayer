9ec4afcf27b19b15aa27f483902a90dc14180885
mahaluo <6709@ait.nsw.edu.au>
2020-06-11 21:13:10 +1000

    toggle project function

--- server.js
+++ server.js
@@ -39,62 +39,62 @@ app.get("/public-projects", cors(), (req, res) => {
     console.log('async init')
 
     let data = firebase
-    .firestore()
-    .collection('projectList')
-    .doc('projects')
-    .collection('users')
-    .get().catch(function (error) {
-      console.error('failed:', error);
-      throw error;
-    }).then(snapshot => {
-      if (snapshot.empty) {
-        console.log('Public found no matching documents.');
-        return;
-      }
-      else {
-        snapshot.forEach(doc => {
+      .firestore()
+      .collection('projectList')
+      .doc('projects')
+      .collection('users')
+      .get().catch(function (error) {
+        console.error('failed:', error);
+        throw error;
+      }).then(snapshot => {
+        if (snapshot.empty) {
+          console.log('Public found no matching documents.');
+          return;
+        }
+        else {
+          snapshot.forEach(doc => {
 
-          let publicRef = firebase
-          .firestore()
-          .collection('projectList')
-          .doc('projects')
-          .collection('users')
-          .doc(doc.id)
-          .collection('userproject');
+            let publicRef = firebase
+              .firestore()
+              .collection('projectList')
+              .doc('projects')
+              .collection('users')
+              .doc(doc.id)
+              .collection('userproject');
 
-          let query = publicRef.get()
-            .then(snapshot => {
-              console.log('user query results..');
-              if (snapshot.empty) {
-                console.log('Public found no matching documents.');
-                return;
-              }
-              snapshot.forEach(doc => {
-                if (doc.data().shared) {
-                  console.log('shared doc found');
-                  console.log(doc.data());
-                  const project = {
-                    project: {
-                      title: doc.data().title,
-                      body: doc.data().body,
-                      createdAt: moment(doc.data().createdAt.toDate()).calendar(),
-                      solved: doc.data().solved,
-                      shared: doc.data().shared,
-                      id: doc.id,
-                    }
-                  }
-                  projectList.push(project);
+            let query = publicRef.get()
+              .then(snapshot => {
+                console.log('user query results..');
+                if (snapshot.empty) {
+                  console.log('Public found no matching documents.');
+                  return;
                 }
-              });
+                snapshot.forEach(doc => {
+                  if (doc.data().shared) {
+                    console.log('shared doc found');
+                    console.log(doc.data());
+                    const project = {
+                      project: {
+                        title: doc.data().title,
+                        body: doc.data().body,
+                        createdAt: moment(doc.data().createdAt.toDate()).calendar(),
+                        solved: doc.data().solved,
+                        shared: doc.data().shared,
+                        id: doc.id,
+                      }
+                    }
+                    projectList.push(project);
+                  }
+                });
 
-              console.log('end of user foreach.. ');
-            })
-            .catch(err => {
-              console.log('Error getting documents', err);
-            }); 
-        });
-      }
-    });
+                console.log('end of user foreach.. ');
+              })
+              .catch(err => {
+                console.log('Error getting documents', err);
+              });
+          });
+        }
+      });
   }
 
   try {
@@ -151,6 +151,65 @@ app.get("/user-projects", cors(), (req, res) => {
     console.log('sending list of projects to client.. ')
 
     res.send(projectList);
+  }
+})
+
+app.get("/toggle-project", cors(), (req, res) => {
+
+  let state = false;
+
+  //toggle boolean state
+  if(req.headers.boolean === 'false') {
+    state = true;
+  }
+
+  try {
+    toggle();
+  } catch (error) {
+    console.error(error)
+  }
+
+  async function toggle() {
+    if (req.headers.sender === 'solved') {
+      firebase
+      .firestore()
+      .collection("projectList")
+      .doc("projects")
+      .collection('users')
+      .doc(req.headers.user)
+      .collection('userproject')
+      .doc(req.headers.id)
+      .update({ 'solved': state })
+      .then(() => {
+        console.log("toggled project solved to: " + state);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.send({ error: err });
+      }).then(() => {
+        res.send({ success: 'success' });
+      });
+    }
+    else {
+      firebase
+      .firestore()
+      .collection("projectList")
+      .doc("projects")
+      .collection('users')
+      .doc(req.headers.user)
+      .collection('userproject')
+      .doc(req.headers.id)
+      .update({ 'shared': state })
+      .then(() => {
+        console.log("toggled project shared to: " + state);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.send({ error: err });
+      }).then(() => {
+        res.send({ success: 'success' });
+      });
+    }
   }
 })
 
@@ -235,28 +294,28 @@ app.get("/post-comment", cors(), (req, res) => {
   }
   try {
     firebase
-    .firestore()
-    .collection('projectList')
-    .doc('projects')
-    .collection('users')
-    .doc(req.headers.user)
-    .collection('userproject')
-    .doc(req.headers.id)
-    .collection('comments')
-    .add({
-      ...newComment
-    })
-    .then(() => {
-      console.log("added comment");
-      res.send({success: 'success'});
-    })
-    .catch((err) => {
-      console.log(err);
-      res.send({error: error})
-    });
+      .firestore()
+      .collection('projectList')
+      .doc('projects')
+      .collection('users')
+      .doc(req.headers.user)
+      .collection('userproject')
+      .doc(req.headers.id)
+      .collection('comments')
+      .add({
+        ...newComment
+      })
+      .then(() => {
+        console.log("added comment");
+        res.send({ success: 'success' });
+      })
+      .catch((err) => {
+        console.log(err);
+        res.send({ error: error })
+      });
   } catch (error) {
     console.error(error);
-    res.send({error: error})
+    res.send({ error: error })
   }
 })
 
